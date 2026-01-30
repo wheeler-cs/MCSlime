79fd3e8c0c173f50c57b91e64f00bde8ebd9979d
Anna <wheelercs@protonmail.com>
2026-01-30 13:41:36 -0600

    Added Docstrings for Java RNG
    Added some docstrings citing the source the RNG algorithms were derived from, along with some information specifying how each of the functions work.
    
    Signed-off-by: Anna <wheelercs@protonmail.com>

--- inc/JavaRNG.h
+++ inc/JavaRNG.h
@@ -4,22 +4,37 @@
 #include <stdint.h>
 
 /**
+ * Java RNG algorithms sourced from Oracle documentation:
+ * https://docs.oracle.com/javase/8/docs/api/java/util/Random.htm
+ */
+
+/**
+ * @param seed Initial seed to use for RNG state.
  * 
+ * @returns Newly-initialzed RNG state.
  */
 uint64_t setSeed(uint64_t);
 
 /**
+ * @param rngState The current RNG state.
+ * @param bits The bit size of the value to return.
  * 
+ * @returns Random value of a specified width.
  */
 uint64_t next(uint64_t *, unsigned int);
 
 /**
+ * @param rngState The current RNG state.
  * 
+ * @returns A 32-bit pseudorandom integer.
  */
 uint64_t nextInt(uint64_t *);
 
 /**
+ * @param rngState The current RNG state.
+ * @param bound The upper bound to place on the value produced.
  * 
+ * @returns A pseudorandom integer between 0 and the bound specified.
  */
 uint64_t nextIntBounded(uint64_t *, uint64_t);
 
